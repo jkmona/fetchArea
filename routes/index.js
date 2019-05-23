5a57a2c1f20f1eec9239364728868e126253e8bb
liuliang <liuliang61958@163.com>
2019-05-23 09:40:50 +0800

    调整数据结构

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const cheerio = require('cheerio');
 const superagent = require('superagent');
 const charset = require('superagent-charset');
+const pinyin = require("node-pinyin");
 var fs = require('fs');
 charset(superagent);
 
@@ -25,17 +26,27 @@ router.post('/doFetchProvince', function(req, response, next) {
     let body = res.text;
     let $ = cheerio.load(body);
     var provinceArray = [];
-    $('.provincetr').each(function(index, elem){
-      $(elem).find("td").each(function(index, elem){
-        let pa = $(elem).find("a");
+    var order = 1;
+    $('.provincetr').each(function(pIndex, elem){
+      $(elem).find("td").each(function(index, el){
+        let pa = $(el).find("a");
         let href = pa.prop('href');
         let provinceId = href.slice(0, href.indexOf('.'));
+        let provinceName = pa.text();
+        var py = pinyin(provinceName,{style:'normal'});
         provinceArray.push({
           nativeId: provinceId,
-          name: pa.text(),
+          parentId:0,
+          nativeCode: provinceId + "000000000",
+          name: provinceName,
+          shortName: getShortName(provinceName),
+          groupName: py[0].join('').slice(0,1).toUpperCase(),
+          pinyin: py.join(' '),
           level: 1,
+          order: order,
           cityURL: provinceURL.slice(0, provinceURL.lastIndexOf('/')) + "/" + provinceId + ".html"
         });
+        order ++;
       });
     });
     //console.log(provinceArray);
@@ -61,22 +72,30 @@ router.post('/doFetchCity', (req, response, next) => {
     let body = res.text;
     let $ = cheerio.load(body);
     var cityArray = [];
+    var order = 1;
     $('.citytr').each(function(index, elem){
       var aList = $(elem).find("td");
       var firstTd = aList.eq(0).find("a");
       var secondTd = aList.eq(1).find("a");
       var href = firstTd.prop('href');
-      
+      var cityName = secondTd.text()
+      var py = pinyin(cityName,{style:'normal'});
       if(href != null && href.length > 0){
         let cityId = href.slice(href.lastIndexOf('/')+1, href.indexOf('.'))
+
         cityArray.push({
           nativeId: cityId,
           parentId: provinceId,
-          code: firstTd.text(),
-          name: secondTd.text(),
+          nativeCode: firstTd.text(),
+          name: cityName,
+          shortName: getShortName(cityName),
+          groupName: py[0].join('').slice(0,1).toUpperCase(),
+          pinyin: py.join(' '),
           level: 2,
+          order: order,
           countyURL: cityURL.slice(0, cityURL.lastIndexOf('.'))  + "/" + cityId + ".html"
         });
+        order ++;
       }else{
         console.log("city 异常连接：" + $(elem).text());
       }
@@ -111,27 +130,41 @@ router.post('/doFetchCounty', (req, response, next) => {
       var secondTd = aList.eq(1).find("a");
       var href = firstTd.prop('href');
       let countyId;
+      var order = 1;
       if(href != null && href.length > 0){
         countyId = href.slice(href.lastIndexOf('/')+1, href.indexOf('.'));
+        let countyName = secondTd.text();
+        var py = pinyin(cityName,{style:'normal'});
         countyArray.push({
           nativeId: countyId,
           parentId: cityId,
-          code: firstTd.text(),
-          name: secondTd.text(),
+          nativeCode: firstTd.text(),
+          name: countyName,
+          shortName: getShortName(countyName),
+          groupName: py[0].join('').slice(0,1).toUpperCase(),
+          pinyin: py.join(' '),
           level: 3,
+          order: order,
           townURL: countyURL.slice(0, countyURL.lastIndexOf('.'))  + "/" + countyId + ".html"
         });
+        order ++;
       } else {
         let fText = aList.eq(0).text();
-        let sText = aList.eq(1).text();
+        let countyName = aList.eq(1).text();
+        var py = pinyin(countyName, {style:'normal'});
         countyId = fText.slice(0, 6);
         countyArray.push({
           nativeId: countyId,
           parentId: cityId,
-          code: fText,
-          name: sText,
-          level: 3
-        });
+          nativeCode: fText,
+          name: countyName,
+          shortName: getShortName(countyName),
+          groupName:py[0].join('').slice(0,1),
+          pinyin:py.join(' '),
+          level: 3,
+          order: order
+        }); 
+        order ++;
       }
     });
     //console.log(countyArray);
@@ -147,6 +180,21 @@ function writeData(fileName, data) {
       let a = "dfd";
       console.log('写入数据成功!')
   })
+}
+function getShortName(source) {
+  if(0 < source.indexOf('省直辖')||0 < source.indexOf('区直辖')){
+    return '省直辖';
+  } else if(0 < source.indexOf('自治')){
+    if(0 < source.indexOf('内蒙古')) {
+      return '内蒙古';
+    }
+    return source.slice(0,3);
+  } else if(source.indexOf('地区')){
+    return source.slice(0,3);
+  }else {
+    return source.replace(/[省|市|县|区]/g, '');
+  }
+  
 }
 
 module.exports = router;
