1715998e6f5c9beb09994683e15e871bf58d0d9f
Lyon <liuliang61958@163.com>
2019-07-11 15:02:40 +0800

    area实体校验

--- models/area.js
+++ models/area.js
@@ -2,18 +2,18 @@ var mongoose  = require('mongoose');
 var Schema    = mongoose.Schema;
 
 var AreaSchema = Schema({
-    nativeId: Number,
-    parentId: Schema.Types.ObjectId,
-    code: { type: String, default:'00000000000'} ,
-    name: String,
+    nativeId: { type: Number, require: true},
+    parentId: { type: Schema.Types.ObjectId, require: true},
+    code: { type: String, default:'00000000000', require: true} ,
+    name: { type: String, require: true},
     shortName: String,
     groupName: String,
     phonics:  String,
     locale: { type: String, default:'all'} ,
-    type: { type: String, enum: ['COUNTRY', 'PROVINCE', 'CITY', 'DISTRICT', 'TOWN']},
+    type: { type: String, require: true, enum: ['COUNTRY', 'PROVINCE', 'CITY', 'DISTRICT', 'TOWN']},
     valid: { type: Boolean, default: true},
     display: { type: Boolean, default: true},
-    rank: Number,
+    rank: { type: Number, require: true},
     subURL: String
 },{ timestamps: true, collection: 'common_region' });
 
